71bf9b40b3ec259609a717349dcac846f2161fb2
huzaifa-turing-tech <huzaifa@AlisMac-Book-Pro.local>
2023-07-10 12:37:37 +0500

    add decorators for Header

--- src/external/twilio/controllers/twilio.controller.ts
+++ src/external/twilio/controllers/twilio.controller.ts
@@ -1,32 +1,34 @@
-import { Controller, Post, Res, Req } from '@nestjs/common';
+import { Controller, Post, Res, Req, Header } from '@nestjs/common';
 import { TwilioService } from '../services/twilio.service';
 import { Request, Response } from 'express';
 
 @Controller('twilio')
 export class TwilioController {
   constructor(private twilioService: TwilioService) {}
+
   @Post('connect')
+  @Header('Content-Type', 'text/xml')
   connect(@Res() res: Response) {
     const callResponse = this.twilioService.handleConnect();
 
-    res.type('text/xml');
-    return res.send(callResponse);
+    return callResponse;
   }
 
   @Post('input')
+  @Header('Content-Type', 'text/xml')
   input(@Res() res: Response, @Req() req: Request) {
     const { Digits } = req.body;
 
     const inputResponse = this.twilioService.handleInput(Digits);
 
-    res.type('text/xml');
-    return res.send(inputResponse);
+    return inputResponse;
   }
 
   @Post('end')
+  @Header('Content-Type', 'text/xml')
   end(@Res() res: Response) {
     const response = this.twilioService.createEndCallRequest();
-    res.type('text/xml');
-    return res.status(200).send(response);
+
+    return response;
   }
 }
